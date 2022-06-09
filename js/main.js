c535e9024c951925a2c26f4fdaa2b96764b030ef
DIma <10d.danenko@gmail.com>
2022-06-09 20:49:39 +0300

    Сделал секцию слайдера, секцию стоимости занятий

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-const swiper = new Swiper(".about-swiper", { // инициализируем слайдер 
+const SwiperAbout = new Swiper(".about-swiper", { // инициализируем слайдер 
     effect: "fade", // 'плавно' скрывает предыдущий слайд 
     navigation: { // стрелки навигации 
         nextEl: ".swiper-button-next", // стрелка левая
         prevEl: ".swiper-button-prev", // стрелка правая 
+    },
+});
+
+
+const SwiperHale = new Swiper(".hall-swiper", { // инициализируем слайдер 
+    slidesPerView: 2, // показывает 2 слайда на экране
+    spaceBetween: 30, // отступы между слайдами
+    centeredSlides: true,
+    initialSlide: 1, // показ слайда с определённого 
+    loop: true, // бесконечно повторение
+    speed: 1000, // Продолжительность перехода между слайдами
+    navigation: { // стрелки навигации 
+        nextEl: ".swiper-button-next", // стрелка левая
+        prevEl: ".swiper-button-prev", // стрелка правая 
+    },
+    pagination: {
+        el: ".swiper-pagination",
     },
 });
